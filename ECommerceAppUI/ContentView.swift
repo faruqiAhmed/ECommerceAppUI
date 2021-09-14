b3b663d70e75af684d0418fd33aa5329fa3c0260
Faruq <faruqdeveloper@gmail.com>
2021-09-14 12:06:05 +0600

    Initial Commit

---
+++ ECommerceAppUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECommerceAppUI
+//
+//  Created by Md Omar Faruq on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ECommerceAppUI/ECommerceAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppUIApp.swift
+//  ECommerceAppUI
+//
+//  Created by Md Omar Faruq on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
